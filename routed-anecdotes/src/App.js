f3d110c8ea09a5815746d0d97d3c8931a3c288b2
Tim Petrvalsky <tpet55@yahoo.com>
2021-11-04 09:07:02 -0700

    Add reset button to fthe form and create a custom hook for the operation of the form

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -9,6 +9,7 @@ import {
   useHistory,
   Redirect
 } from "react-router-dom";
+import { useField } from "./hooks";
 
 const Menu = () => {
   const padding = {
@@ -100,18 +101,25 @@ const Notification = ({ anecdote }) => {
 };
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     });
+  };
+
+  const handleReset = (e) => {
+    // e.preventDefault();
+    content.onReset();
+    author.onReset();
+    info.onReset();
   };
 
   return (
@@ -120,30 +128,19 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button onClick={handleSubmit}>create</button>
       </form>
+      <button onClick={handleReset}>reset</button>
     </div>
   );
 };
